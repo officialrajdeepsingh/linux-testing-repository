32f785c6c15018904e6573fc25e49a90fed785ff
Rajdeep singh <radewala.2@gmail.com>
2023-08-20 13:28:49 +0000

    update the test repo

--- shadnui/src/app/page.tsx
+++ shadnui/src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
-
+import {Alert, AlertTitle, AlertDescription} from "../components/ui/alert"
+import { Button, buttonVariants } from "../components/ui/button"
 export default function Home() {
   return (
     <main className="">
-
+      <Button variant="destructive">Destructive</Button>
+<Alert>
+  <AlertTitle>Heads up!</AlertTitle>
+  <AlertDescription>
+    You can add components and dependencies to your app using the cli.
+  </AlertDescription>
+</Alert>
     </main>
   )
 }
